91b28c3b140a05240b9886ef3e457c208ccacb7b
Bob Gregory <bob.gregory@cazoo.co.uk>
2022-06-29 12:40:54 +0100

    Html is now in a writer

--- expensereport-typescript/src/ExpenseReport.ts
+++ expensereport-typescript/src/ExpenseReport.ts
@@ -4,8 +4,8 @@ class Expense {
   constructor(public type: ExpenseType, public amount: number) {
   }
 
-  get name() : string {
-    switch(this.type) {
+  get name(): string {
+    switch (this.type) {
       case "dinner":
         return "Dinner"
       case "breakfast":
@@ -15,15 +15,15 @@ class Expense {
     }
   }
 
-  get isOverLimits() : boolean {
-    return this.type == "dinner" && this.amount > 5000 || this.type == "breakfast" && this.amount > 1000 ? true: false
+  get isOverLimits(): boolean {
+    return this.type == "dinner" && this.amount > 5000 || this.type == "breakfast" && this.amount > 1000 ? true : false
   }
 }
 
 class Expenses implements Iterable<Expense> {
-  constructor (private items: Array<Expense>){}
+  constructor(private items: Array<Expense>) { }
 
-  [Symbol.iterator]() : Iterator<Expense> {
+  [Symbol.iterator](): Iterator<Expense> {
     return this.items[Symbol.iterator]();
   }
 
@@ -36,14 +36,20 @@ class Expenses implements Iterable<Expense> {
   }
 }
 
-class PlaintextWriter {
+interface ExpensesWriter {
+  writeHeader(): void
+  writeItems(expenses: Expenses): void
+  writeTotal(expenses: Expenses): void
+}
+
+class PlaintextWriter implements ExpensesWriter {
   writeHeader() {
     process.stdout.write("Expense Report: " + new Date().toISOString().substr(0, 10) + "\n")
   }
 
   writeItems(expenses: Iterable<Expense>) {
     for (const expense of expenses) {
-      process.stdout.write(expense.name + "\t" + expense.amount + "\t" + (expense.isOverLimits? "X": " ") + "\n")
+      process.stdout.write(expense.name + "\t" + expense.amount + "\t" + (expense.isOverLimits ? "X" : " ") + "\n")
     }
   }
 
@@ -53,17 +59,8 @@ class PlaintextWriter {
   }
 }
 
-function printReport(htmlMode: boolean, _expenses: Expense[]) {
-  const expenses = new Expenses(_expenses);
-  const writer = new PlaintextWriter();
-
-  if(!htmlMode){
-    writer.writeHeader()
-    writer.writeItems(expenses)
-    writer.writeTotal(expenses)
-    return
-  }
-
+class HtmlWriter {
+  writeHeader() {
     process.stdout.write("<!DOCTYPE html>\n");
     process.stdout.write("<html>\n");
     process.stdout.write("<head>\n");
@@ -71,13 +68,14 @@ function printReport(htmlMode: boolean, _expenses: Expense[]) {
     process.stdout.write("</head>\n");
     process.stdout.write("<body>\n");
     process.stdout.write("<h1>Expense Report: " + new Date().toISOString().substr(0, 10) + "</h1>\n")
+  }
 
+  writeItems(expenses: Expenses) {
     process.stdout.write("<table>\n");
     process.stdout.write("<thead>\n");
     process.stdout.write("<tr><th scope=\"col\">Type</th><th scope=\"col\">Amount</th><th scope=\"col\">Over Limit</th></tr>\n");
     process.stdout.write("</thead>\n");
     process.stdout.write("<tbody>\n");
-
 
     for (const expense of expenses) {
       const mealOverExpensesMarker = expense.isOverLimits ? "X" : " "
@@ -85,12 +83,25 @@ function printReport(htmlMode: boolean, _expenses: Expense[]) {
     }
     process.stdout.write("</tbody>\n");
     process.stdout.write("</table>\n");
+  }
 
+  writeTotal(expenses: Expenses) {
     process.stdout.write("<p>Meal Expenses: " + expenses.mealExpenses + "</p>\n")
     process.stdout.write("<p>Total Expenses: " + expenses.totalExpenses + "</p>\n")
 
     process.stdout.write("</body>\n");
     process.stdout.write("</html>\n");
+  }
 }
 
-export {printReport, Expense, ExpenseType}
+function printReport(htmlMode: boolean, _expenses: Expense[]) {
+  const expenses = new Expenses(_expenses);
+  const writer = htmlMode ? new HtmlWriter() : new PlaintextWriter();
+
+    writer.writeHeader()
+    writer.writeItems(expenses)
+    writer.writeTotal(expenses)
+
+}
+
+export { printReport, Expense, ExpenseType }
